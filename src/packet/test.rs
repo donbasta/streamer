da23fc086131fdd6db56b487b6c51b758d56b7c8
donbasta <faddilafarras@gmail.com>
2022-10-20 02:30:57 +0700

    refactor: move struct declaration from mod to item

--- src/packet/test.rs
+++ src/packet/test.rs
@@ -1,16 +1,12 @@
 use bytes::{BufMut, BytesMut};
 
-use crate::packet::Packet;
+use crate::packet::item::Packet;
 
 #[test]
 fn test_head() {
     let mut bytes_mut = BytesMut::new();
     bytes_mut.put(&b"abcd"[..]);
-    let packet = Packet {
-        p_type: 0usize,
-        p_sequence: 2usize,
-        p_data: bytes_mut,
-    };
+    let packet = Packet::new(0usize, 2usize, &bytes_mut);
 
     assert_eq!(&b"\x00\x00\x04\x00\x02"[..], packet.generate_head());
 }
@@ -19,11 +15,7 @@ fn test_head() {
 fn test_checksum() {
     let mut bytes_mut = BytesMut::new();
     bytes_mut.put(&b"abcd"[..]);
-    let packet = Packet {
-        p_type: 0usize,
-        p_sequence: 2usize,
-        p_data: bytes_mut,
-    };
+    let packet = Packet::new(0usize, 2usize, &bytes_mut);
 
     assert_eq!(2, packet.generate_checksum(&mut packet.generate_head()));
 }
@@ -32,11 +24,7 @@ fn test_checksum() {
 fn test_to_bytes() {
     let mut bytes_mut = BytesMut::new();
     bytes_mut.put(&b"abcd"[..]);
-    let packet = Packet {
-        p_type: 0usize,
-        p_sequence: 2usize,
-        p_data: bytes_mut,
-    };
+    let packet = Packet::new(0usize, 2usize, &bytes_mut);
 
     assert_eq!(
         BytesMut::from(&b"\x00\x00\x04\x00\x02\x00\x02abcd"[..]),
@@ -49,7 +37,7 @@ fn test_to_packet() {
     let raw = BytesMut::from(&b"\x00\x00\x04\x00\x02\x00\x02abcd"[..]);
     let packet = Packet::to_packet(raw).unwrap();
 
-    assert_eq!(0, packet.p_type);
-    assert_eq!(2, packet.p_sequence);
-    assert_eq!(&b"abcd"[..], packet.p_data);
+    assert_eq!(0, packet.get_type());
+    assert_eq!(2, packet.get_sequence());
+    assert_eq!(&b"abcd"[..], packet.get_data());
 }
