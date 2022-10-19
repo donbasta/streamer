a8d9f1de9538eecf16d044b15fe5d58a45495585
donbasta <faddilafarras@gmail.com>
2022-10-19 23:47:28 +0700

    fix: add bytes crate to handle bytes in packet

--- src/constant.rs
+++ src/constant.rs
@@ -5,30 +5,29 @@ pub const MAX_LENGTH: usize = 32767;
 pub const MAX_SEQUENCE: usize = 65535;
 pub const MAX_PACKET_LENGTH: usize = 1 + 2 + 2 + 2 + 32767;
 
-const TYPE_DATA: usize = 0;
-pub const TYPE_ACK: usize = 1;
-const TYPE_FIN: usize = 2;
-pub const TYPE_FINACK: usize = 3;
-const TYPE_MDATA: usize = 4;
-const TYPE_MACK: usize = 5;
+pub const TYPE_DATA: usize = 1;
+pub const TYPE_ACK: usize = 2;
+pub const TYPE_FIN: usize = 3;
+pub const TYPE_FINACK: usize = 4;
+pub const TYPE_MDATA: usize = 5;
+pub const TYPE_MACK: usize = 6;
 
-// const AVAILABLE_TYPES: Vec<usize> = if FILE_METADATA {
-//     [
-//         TYPE_DATA,
-//         TYPE_ACK,
-//         TYPE_FIN,
-//         TYPE_FINACK,
-//         TYPE_MDATA,
-//         TYPE_MACK,
-//     ]
-//     .to_vec()
-// } else {
-//     [TYPE_DATA, TYPE_ACK, TYPE_FIN, TYPE_FINACK].to_vec()
-// };
+pub const AVAILABLE_TYPES: [usize; 6] = if FILE_METADATA {
+    [
+        TYPE_DATA,
+        TYPE_ACK,
+        TYPE_FIN,
+        TYPE_FINACK,
+        TYPE_MDATA,
+        TYPE_MACK,
+    ]
+} else {
+    [TYPE_DATA, TYPE_ACK, TYPE_FIN, TYPE_FINACK, 0, 0]
+};
 
-const MAX_SENDER_THREADS: usize = if MULTITHREADED { 5 } else { 1 };
-const RESEND_TIMER: usize = 2;
-const RESEND_TIMER_TICK: usize = 2;
+pub const MAX_SENDER_THREADS: usize = if MULTITHREADED { 5 } else { 1 };
+pub const RESEND_TIMER: usize = 2;
+pub const RESEND_TIMER_TICK: usize = 2;
 
-const CHUNK_SIZE: usize = 32767;
-const STOP_MESSAGE: &str = "BerhentiDong";
+pub const CHUNK_SIZE: usize = 32767;
+pub const STOP_MESSAGE: &str = "BerhentiDong";
